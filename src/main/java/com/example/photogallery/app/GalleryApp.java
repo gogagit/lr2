78ebc9e88cc2cb2c3a5e61e07ddafcc370e93f3a
Илюша <ilyasemenyt@gmail.com>
2026-04-27 15:55:47 +0300

    Fix v3

--- src/main/java/com/example/photogallery/app/GalleryApp.java
+++ src/main/java/com/example/photogallery/app/GalleryApp.java
@@ -11,23 +11,26 @@ public class GalleryApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         URL fxmlUrl = getClass().getResource("/fxml/gallery-view.fxml");
+
         if (fxmlUrl == null) {
             System.err.println("FXML file not found!");
             return;
         }
 
         FXMLLoader loader = new FXMLLoader(fxmlUrl);
-        Scene scene = new Scene(loader.load(), 1240, 940);
+
+        Scene scene = new Scene(loader.load(), 1180, 760);
 
         URL cssUrl = getClass().getResource("/css/gallery.css");
+
         if (cssUrl != null) {
             scene.getStylesheets().add(cssUrl.toExternalForm());
         }
 
         stage.setTitle("Альбом впечатлений");
         stage.setScene(scene);
-
-        
+        stage.setMinWidth(1080);
+        stage.setMinHeight(700);
         stage.show();
     }
 
